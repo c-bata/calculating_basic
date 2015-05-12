736a8456ac2785ecd1762d4208e4420084cd1b69
c-bata <contact@c-bata.link>
2015-05-12 11:53:49 +0900

    Modify kadai1's report more

--- src/kadai1-1.c
+++ src/kadai1-1.c
@@ -2,9 +2,9 @@
 #include <math.h>
 #include <stdlib.h>
 
-void quadratic_formula(double a, double b, double c){
-    double discriminant = b * b - 4 * a * c;
-    double ans1, ans2;
+void quadratic_formula(float a, float b, float c){
+    float discriminant = b * b - 4 * a * c;
+    float ans1, ans2;
 
     if (discriminant > 0){
         ans1 = ( -b + sqrt(discriminant) ) / (2*a);
@@ -14,41 +14,50 @@ void quadratic_formula(double a, double b, double c){
         ans1 = -b / ( 2 * a );
         printf("2次方程式の解: x = %f (2重解)です\n",ans1);
     }else{
-        double real = -b / (2*a);
-        double imag = sqrt(-discriminant) / (2*a);
+        float real = -b / (2*a);
+        float imag = sqrt(-discriminant) / (2*a);
         printf("2次方程式の解: x = %.2f+%.2fi, %.2f-%.2fです\n", real, imag, real, imag);
     }
-
 }
 
-void new_quadratic_formula(double a, double b, double c){
-    double discriminant = b * b - 4 * a * c;
-    double ans1, ans2;
+
+float calc_other_quadratic_formula(float a, float c, float x){
+    return c / (a * x);
+}
+
+void new_quadratic_formula(float a, float b, float c){
+    float discriminant = b * b - 4 * a * c;
+    float ans1, ans2;
 
     if(discriminant > 0){
-        ans1 = (-2*c) / (b + sqrt(b*b - 4*a*c));
-        ans2 = (2*c) / (-b + sqrt(b*b - 4*a*c));
+        if(b < 0){
+            ans1 = ( -b + sqrt(discriminant) ) / (2*a);
+            ans2 = calc_other_quadratic_formula(a, c, ans1);
+        } else {
+            ans2 = ( -b - sqrt(discriminant) ) / (2*a);
+            ans1 = calc_other_quadratic_formula(a, c, ans2);
+        }
         printf("2次方程式の解: x = %f, %fです\n",ans1 ,ans2);
     }else if (discriminant == 0){
-        ans1 = (-2 * c) / b;
+        ans1 = -b / ( 2 * a );
         printf("2次方程式の解: x = %f (2重解)です\n",ans1);
     }else{
-        double real = -b / (2*a);
-        double imag = sqrt(-discriminant) / (2*a);
+        float real = -b / (2*a);
+        float imag = sqrt(-discriminant) / (2*a);
         printf("2次方程式の解: x = %.2f+%.2fi, %.2f-%.2fです\n", real, imag, real, imag);
     }
 }
 
 int main(void){
-    double a, b, c;
+    float a, b, c;
 
     printf("係数を入力して下さい。\n");
     printf("a > ");
-    scanf("%lf",&a);
+    scanf("%f",&a);
     printf("b > ");
-    scanf("%lf",&b);
+    scanf("%f",&b);
     printf("c > ");
-    scanf("%lf",&c);
+    scanf("%f",&c);
 
     // 解の公式
     printf("\nそのまま計算した場合\n");
